c696ca70d77a002bc54c243ae1cfdaf73f4e7602
Shreyasi <shreyasi18sia@gmail.com>
2021-07-30 11:53:46 +0530

    Created Instances

--- Inheritance/src/main/kotlin/main.kt
+++ Inheritance/src/main/kotlin/main.kt
@@ -1,4 +1,16 @@
 fun main()
 {
+    // Creating instances for class Dogs
+    val dog = Dogs(4, "Brown")
+    dog.bark()
+    dog.sleep()
+    dog.eat()
 
+    println()
+    
+    // Creating instance for class Cats
+    val cat = Cats(4, "White")
+    cat.meow()
+    cat.sleep()
+    cat.eat()
 }
